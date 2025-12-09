5f334539f519ebf5dd29d71b4235a9c6afc33a9f
Geraetefreund <friteuse@arcor.de>
2025-12-09 08:25:25 +0000

    stuck

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -12,3 +12,23 @@ RETURNING *;
 
 -- name: GetFeeds :many
 SELECT * FROM feeds;
+
+-- name: CreateFeedFollow :one
+WITH inserted_feed_follow AS (
+  INSERT INTO feed_follows (id, created_at, updated_at, user_id, feed_id)
+  VALUES (
+    GEN_RANDOM_UUID(),
+    NOW(),
+    NOW(),
+    $1,
+    $2
+    )
+  RETURNING *
+)
+SELECT 
+  inserted_feed_follow.*,
+  feeds.name AS feed_name,
+  users.name AS user_name
+FROM inserted_feed_follow
+INNER JOIN inserted_feed_follow.user_id ON users.id = inserted_feed_follow.user_id
+INNER JOIN inserted_feed_follow.feed_id ON feeds.id = inserted_feed_follow.feed_id ;
